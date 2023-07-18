53dc97161ec9a68966e13346cc7a6dbe7b0da81c
Валерия Русакова <rusakovalera@gmail.com>
2023-07-18 19:57:03 +0300

    Задание 3. Сделайте сценарий логина

---
+++ fixture/application.py
@@ -0,0 +1,50 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import allure
+from fixture.session import SessionHelper
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class Application:
+    def __init__(self, browser,base_url):
+
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Не указан браузер")
+
+
+        self.vars = {}
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def destroy(self):
+        self.wd.quit()
+
+    def open_home_page(self):
+        wd = self.wd
+        with allure.step('Открытие страницы litecart'):
+            wd.get(self.base_url)
+        with allure.step('Открытие окна браузера на полный экран'):
+            wd.set_window_size(2000, 1100)
+
+    def open_admin_page(self):
+        wd = self.wd
+        with allure.step('Открытие страницы админа'):
+            wd.get("http://localhost/litecart/public_html/admin/")
+        with allure.step('Открытие окна браузера на полный экран'):
+            wd.set_window_size(2000, 1100)
+        WebDriverWait(wd, 10).until(lambda x: x.find_element(By.NAME, "login"))
+
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
---
+++ fixture/session.py
@@ -0,0 +1,71 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import allure
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+class SessionHelper:
+    def __init__(self, app):
+        self.app = app
+
+    def login_in_catalog(self, username, password):
+        wd = self.app.wd
+        self.app.open_home_page()
+        with allure.step('Ввести логин'):
+            wd.find_element(By.NAME, "email").clear()
+            wd.find_element(By.NAME, "email").send_keys(username)
+        with allure.step('Ввести пароль'):
+            wd.find_element(By.NAME, "password").clear()
+            wd.find_element(By.NAME, "password").send_keys(password)
+        with allure.step('Нажать кнопку Login'):
+            wd.find_element(By.NAME, 'login').click()
+
+    def login_in_admin_panel(self, username, password):
+        wd = self.app.wd
+        self.app.open_admin_page()
+        with allure.step('Ввести логин'):
+            wd.find_element(By.NAME, "username").clear()
+            wd.find_element(By.NAME, "username").send_keys(username)
+        with allure.step('Ввести пароль'):
+            wd.find_element(By.NAME, "password").clear()
+            wd.find_element(By.NAME, "password").send_keys(password)
+        with allure.step('Нажать кнопку Login'):
+            wd.find_element(By.NAME, 'login').click()
+
+
+
+    def logout_from_catalog(self):
+        wd = self.app.wd
+        with allure.step('Нажать на ссылку Logout'):
+            wd.find_element(By.LINK_TEXT, "Logout").click()
+        WebDriverWait(wd, 10).until(lambda x: x.find_element(By.NAME, "login"))
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout_from_catalog()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements(By.LINK_TEXT, "Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        user = self.get_logged_user()
+        return user == username
+
+    def get_logged_user(self):
+        wd = self.app.wd
+        wd.find_element(By.LINK_TEXT, "Edit Account").click()
+
+        user = wd.find_element(By.NAME, "email").text
+        return user
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout_from_catalog()
+        self.login_in_catalog(username, password)
